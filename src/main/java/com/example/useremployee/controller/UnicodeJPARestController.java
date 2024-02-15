e4221ca0a8d941420d023ac3e5d41f180975146f
MarkusIngerslev <markusolsen2004@gmail.com>
2024-02-15 18:24:34 +0100

    Step 3  lavet, der er nu et endpoint ther modtager en char og returnerer unicode!

--- src/main/java/com/example/useremployee/controller/UnicodeJPARestController.java
+++ src/main/java/com/example/useremployee/controller/UnicodeJPARestController.java
@@ -7,9 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UnicodeJPARestController {
 
+    // Add a new method that takes an int as a parameter
+    // and returns a string with the int and its char value
     @GetMapping("/unicode/{i}")
     public String unicodeToChar(@PathVariable int i) {
         char c = (char)i;
         return "unicode = " + i + " char = " + c;
+    }
+
+    // Add a new method that takes a char as a parameter
+    // and returns a string with the char and its unicode value
+    @GetMapping("/char/{c}")
+    public String charToUnicode(@PathVariable char c) {
+        int i = (int)c;
+        return "char = " + c + " unicode = " + i;
     }
 }
